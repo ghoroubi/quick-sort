550e2352fc3341c1aaebe5245f4e612fe99c8df9
Nima Ghoroubi <ghoroubi85@gmail.com>
2019-09-26 11:39:50 +0330

    init

---
+++ sort.go
@@ -0,0 +1,110 @@
+package quick_sort
+
+import (
+	"reflect"
+	"strings"
+)
+
+// Default sorting order
+var asc = true
+
+// package entry point for external usage
+func QuickSort(arr []interface{}, ascending bool) []interface{} {
+	asc = ascending
+	cloned := make([]interface{}, len(arr))
+	
+	for i, v := range arr {
+		cloned[i] = v
+	}
+	
+	sort(cloned, 0, len(arr)-1)
+	
+	return cloned
+}
+
+// return true if i<j
+func less(i, j interface{}, asc bool) bool {
+	switch i.(type) {
+	case string:
+		
+		if asc {
+			return strings.Compare(reflect.ValueOf(i).String(), reflect.ValueOf(j).String()) == -1
+		} else {
+			return strings.Compare(reflect.ValueOf(i).String(), reflect.ValueOf(j).String()) == 1
+		}
+	case float64:
+		if asc {
+			return reflect.ValueOf(i).Float() < reflect.ValueOf(j).Float()
+		} else {
+			return reflect.ValueOf(i).Float() > reflect.ValueOf(j).Float()
+		}
+	case float32:
+		if asc {
+			return reflect.ValueOf(i).Float() < reflect.ValueOf(j).Float()
+		} else {
+			return reflect.ValueOf(i).Float() > reflect.ValueOf(j).Float()
+		}
+	case int:
+		if asc {
+			return reflect.ValueOf(i).Int() < reflect.ValueOf(j).Int()
+		} else {
+			return reflect.ValueOf(i).Int() > reflect.ValueOf(j).Int()
+		}
+	case int8:
+		if asc {
+			return reflect.ValueOf(i).Int() < reflect.ValueOf(j).Int()
+		} else {
+			return reflect.ValueOf(i).Int() > reflect.ValueOf(j).Int()
+		}
+	case int16:
+		if asc {
+			return reflect.ValueOf(i).Int() < reflect.ValueOf(j).Int()
+		} else {
+			return reflect.ValueOf(i).Int() > reflect.ValueOf(j).Int()
+		}
+	case int32:
+		if asc {
+			return reflect.ValueOf(i).Int() < reflect.ValueOf(j).Int()
+		} else {
+			return reflect.ValueOf(i).Int() > reflect.ValueOf(j).Int()
+		}
+	case int64:
+		if asc {
+			return reflect.ValueOf(i).Int() < reflect.ValueOf(j).Int()
+		} else {
+			return reflect.ValueOf(i).Int() > reflect.ValueOf(j).Int()
+		}
+		
+	}
+	return false
+}
+
+// sort
+// main logic of quickSort
+func sort(arr []interface{}, start, end int) {
+	if (end - start) < 1 {
+		return
+	}
+	
+	pivot := arr[end]
+	splitter := start
+	
+	for i := start; i < end; i++ {
+		if less(arr[i], pivot, asc) {
+			// swap
+			arr[i], arr[splitter] = arr[splitter], arr[i]
+			
+			/*temp := arr[splitter]
+			arr[splitter] = arr[i]
+			arr[i] = temp*/
+			
+			splitter++
+		}
+	}
+	
+	arr[end] = arr[splitter]
+	arr[splitter] = pivot
+	
+	sort(arr, start, splitter-1)
+	sort(arr, splitter+1, end)
+}
---
+++ sort_test.go
@@ -0,0 +1,60 @@
+package quick_sort
+
+import (
+	"math/rand"
+	"reflect"
+	"testing"
+	"time"
+)
+
+var (
+	strArr = []interface{}{
+		"c", "a", "z", "i", "b",
+	}
+	intArr = []interface{}{8, 5, 7, 6, 1, 3}
+	
+	floatArr    = []interface{}{8.9, 5.12, 0.314, 3.1415, 1.7058}
+	sortedStr   = []string{"z", "i", "c", "b", "a"}
+	sortedInt   = []int{8, 7, 6, 5, 3, 1}
+	sortedFloat = []float64{0.314, 1.7058, 3.1415, 5.12, 8.9}
+)
+
+func TestQuickSort(t *testing.T) {
+	
+	newStrArr := QuickSort(strArr, false)
+	newIntArr := QuickSort(intArr, false)
+	newFloatArr := QuickSort(floatArr, true)
+	
+	for i, v1 := range newStrArr {
+		if !reflect.DeepEqual(v1, sortedStr[i]) {
+			t.FailNow()
+		}
+	}
+	for i, v1 := range newIntArr {
+		if !reflect.DeepEqual(v1, sortedInt[i]) {
+			t.FailNow()
+		}
+	}
+	for i, v1 := range newFloatArr {
+		if !reflect.DeepEqual(v1, sortedFloat[i]) {
+			t.FailNow()
+		}
+	}
+	
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+	// Fill a random array
+	rand.Seed(time.Now().Unix())
+	randArr := make([]interface{}, 1000)
+	for i := 0; i < 1000; i++ {
+		randArr[i] = rand.Float64() + 1
+	}
+	
+	for i := 0; i < b.N; i++ {
+		QuickSort(randArr, true)
+	
+	}
+	
+	b.ReportAllocs()
+}
